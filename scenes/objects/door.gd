76d2d6d8bcfed0c54cc7a83b6b728fc030e67674
dalifyyy <d4lified@gmail.com>
2026-06-05 19:24:41 +0800

    fix: fixed the path for npc and added door auth

--- scenes/objects/door.gd
+++ scenes/objects/door.gd
@@ -3,9 +3,9 @@ extends Area3D
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") or body.is_in_group("npc") or body.is_in_group("patient"):
 		animation.play("open")
 
 func _on_body_exited(body) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") or body.is_in_group("npc") or body.is_in_group("patient"):
 		animation.play_backwards("open")
