ca97224fb39a766c7d2963de5b30482663e1879d
dalifyyy <d4lified@gmail.com>
2026-06-08 15:49:35 +0800

    feat: add day cpnfig file baseplate

---
+++ scripts/autoloads/days_config.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var day_configs: Array = []
+
+func _ready() -> void:
+	var json = FileAccess.open("res://scripts/data/days.json", FileAccess.READ)
+	day_configs = JSON.parse_string(json.get_as_text())
+
+func get_day_config(number: int = -1) -> Dictionary:
+	var index = number -1
+	if index < day_configs.size():
+		return day_configs[index]
+	else:
+		return day_configs[-1]
