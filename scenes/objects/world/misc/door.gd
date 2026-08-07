fe8f3f488945fc32042490330c9148c68816a60b
dalifyyy <d4lified@gmail.com>
2026-08-07 15:11:12 +0800

    fix: fixed doors open and close collision and animation

--- scenes/objects/world/misc/door.gd
+++ scenes/objects/world/misc/door.gd
@@ -1,16 +1,17 @@
 class_name Door extends Area3D
 
-@onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var door: StaticBody3D = $door_collision
 
+var door_tween: Tween
 var bodies: Array = []
 
 func _on_body_entered(body) -> void:
 	if body is Player or body is NPC or body is Patient:
-		if not animation.is_playing() and bodies.is_empty():
-			animation.play("open")
-		else:
-			await animation.animation_finished
-			animation.play("open")
+		if door_tween and door_tween.is_valid():
+			door_tween.kill()
+		
+		door_tween = get_tree().create_tween()
+		door_tween.tween_property(door, "position:x", 2.39, 0.2)
 		
 		bodies.append(body)
 
@@ -21,9 +22,13 @@ func _on_body_exited(body) -> void:
 func _remove_body(body: Node) -> void:
 	if bodies.has(body):
 		bodies.erase(body)
-
-	if bodies.is_empty() and not animation.is_playing():
-		animation.play_backwards("open")
+	
+	if bodies.is_empty():
+		if door_tween and door_tween.is_valid():
+			door_tween.kill()
+		
+		door_tween = get_tree().create_tween()
+		door_tween.tween_property(door, "position:x", 0.0, 0.2)
 
 func _physics_process(_delta: float) -> void:
 	if bodies.is_empty():
--- scenes/objects/world/misc/double_door.gd
+++ scenes/objects/world/misc/double_door.gd
@@ -1,16 +1,20 @@
 class_name DoubleDoor extends Area3D
 
-@onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var door_1: StaticBody3D = $StaticBody3D
+@onready var door_2: StaticBody3D = $StaticBody3D2
 
+var door_tween: Tween
 var bodies: Array = []
 
 func _on_body_entered(body) -> void:
 	if body is Player or body is NPC or body is Patient:
-		if not animation.is_playing() and bodies.is_empty():
-			animation.play("open")
-		else:
-			await animation.animation_finished
-			animation.play("open")
+		if door_tween and door_tween.is_valid():
+			door_tween.kill()
+			
+		door_tween = get_tree().create_tween()
+		door_tween.tween_property(door_1, "position:x", -2.9, 0.2)
+		door_tween.parallel()
+		door_tween.tween_property(door_2, "position:x", 2.9, 0.2)
 		
 		bodies.append(body)
 
@@ -22,8 +26,14 @@ func _remove_body(body: Node) -> void:
 	if bodies.has(body):
 		bodies.erase(body)
 
-	if bodies.is_empty() and not animation.is_playing():
-		animation.play_backwards("open")
+	if bodies.is_empty():
+		if door_tween and door_tween.is_valid():
+			door_tween.kill()
+			
+		door_tween = get_tree().create_tween()
+		door_tween.tween_property(door_1, "position:x", -0.9, 0.2)
+		door_tween.parallel()
+		door_tween.tween_property(door_2, "position:x", 0.9, 0.2)
 
 func _physics_process(_delta: float) -> void:
 	if bodies.is_empty():
